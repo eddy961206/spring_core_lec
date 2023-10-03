1c6c7471cf0f64e7de616e8a2ae3960db215afce
skykum <skykum2018@gmail.com>
2023-10-03 21:29:57 +0900

    스프링 핵심 원리 - 기본편 57강 '프로토타입 스코프'

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,45 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("PrototypeBean1 = " + prototypeBean1);
+        System.out.println("PrototypeBean2 = " + prototypeBean2);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        prototypeBean1.destory();   // 프로토타입 빈은 수동으로 직접 destory 해줘야 함.
+        prototypeBean2.destory();
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destory() {
+            System.out.println("PrototypeBean.destory");
+        }
+
+    }
+
+}

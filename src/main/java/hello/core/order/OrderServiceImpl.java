8cef81827ee53cacc6386afa39fadc70861d0e43
skykum <skykum2018@gmail.com>
2023-10-03 14:03:30 +0900

    스프링 핵심 원리 - 기본편 49강 어노테이션 직접 만들기 까지.

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,51 @@
+package hello.core.order;
+
+import hello.core.annotation.MainDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+//@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+//    @Autowired  // 필드 주입
+//    private MemberRepository memberRepository;
+//    @Autowired
+//    private DiscountPolicy discountPolicy;
+
+//    @Autowired    // setter 주입
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    @Autowired  // 생성자 주입 (@RequiredArgsConstructor 어노테이션이 주입해줌)
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 설계가 잘 된것. 왜냐면 할인 관련된건 discountPolicy가 알아서 다 해주니까. 단일책임원칙.
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+}
